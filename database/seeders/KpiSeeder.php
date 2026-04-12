2df2080459e26e8526770cfd343b0b844c54fdec
Ripr Lutuk <rizqy.pratama85@gmail.com>
2026-04-12 15:29:12 +0700

    feat: Updated KpiSeeder to include KPI Groups hierarchy dynamically

--- database/seeders/KpiSeeder.php
+++ database/seeders/KpiSeeder.php
@@ -3,43 +3,102 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\KpiGroup;
 use App\Models\KpiTemplate;
 
 class KpiSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        $kpis = [
-            [
-                'name' => 'Pencapaian Target / Productivity',
-                'weight' => 40,
-                'is_active' => true,
-            ],
-            [
-                'name' => 'Tanggung Jawab / Responsibility',
-                'weight' => 30,
-                'is_active' => true,
-            ],
-            [
-                'name' => 'Kerja Sama / Teamwork',
-                'weight' => 15,
-                'is_active' => true,
-            ],
-            [
-                'name' => 'Komunikasi / Communication',
-                'weight' => 15,
-                'is_active' => true,
-            ]
-        ];
+        $group = KpiGroup::firstOrCreate([
+            'name' => 'Indikator Kinerja Utama'
+        ], [
+            'weight' => 50,
+            'is_active' => true,
+        ]);
 
-        foreach ($kpis as $kpi) {
-            KpiTemplate::firstOrCreate(
-                ['name' => $kpi['name']],
-                $kpi
-            );
-        }
+        KpiTemplate::firstOrCreate([
+            'kpi_group_id' => $group->id,
+            'name' => 'Pencapaian Target / Productivity'
+        ], [
+            'indicator_description' => '',
+            'weight' => 40,
+            'is_active' => true,
+        ]);
+
+        KpiTemplate::firstOrCreate([
+            'kpi_group_id' => $group->id,
+            'name' => 'Tanggung Jawab / Responsibility'
+        ], [
+            'indicator_description' => '',
+            'weight' => 30,
+            'is_active' => true,
+        ]);
+
+        KpiTemplate::firstOrCreate([
+            'kpi_group_id' => $group->id,
+            'name' => 'Kerja Sama / Teamwork'
+        ], [
+            'indicator_description' => '',
+            'weight' => 15,
+            'is_active' => true,
+        ]);
+
+        KpiTemplate::firstOrCreate([
+            'kpi_group_id' => $group->id,
+            'name' => 'Komunikasi / Communication'
+        ], [
+            'indicator_description' => '',
+            'weight' => 15,
+            'is_active' => true,
+        ]);
+
+        $group = KpiGroup::firstOrCreate([
+            'name' => 'NIlai Utama Kepegawaian'
+        ], [
+            'weight' => 50,
+            'is_active' => true,
+        ]);
+
+        KpiTemplate::firstOrCreate([
+            'kpi_group_id' => $group->id,
+            'name' => 'Kemampuan Komunikasi'
+        ], [
+            'indicator_description' => '- Menyampaikan pesan			
+- Menerima pesan',
+            'weight' => 25,
+            'is_active' => true,
+        ]);
+
+        KpiTemplate::firstOrCreate([
+            'kpi_group_id' => $group->id,
+            'name' => 'Orientasi Hasil'
+        ], [
+            'indicator_description' => '- Memiliki keinginan untuk mencapai target			
+- Bertanggung jawab untuk mencapai hasil			',
+            'weight' => 25,
+            'is_active' => true,
+        ]);
+
+        KpiTemplate::firstOrCreate([
+            'kpi_group_id' => $group->id,
+            'name' => 'Pengembangan diri'
+        ], [
+            'indicator_description' => '- Kesadaran dan motivasi			
+- Mengembangkan diri			',
+            'weight' => 25,
+            'is_active' => true,
+        ]);
+
+        KpiTemplate::firstOrCreate([
+            'kpi_group_id' => $group->id,
+            'name' => 'Kerjasama Tim'
+        ], [
+            'indicator_description' => '- Mendorong terciptanya kerja sama			
+- Sinergi Tim			',
+            'weight' => 25,
+            'is_active' => true,
+        ]);
+
     }
 }
